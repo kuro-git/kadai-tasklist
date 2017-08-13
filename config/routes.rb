924ed72444f06c8cd21e52440d743c2ac49e36a1
kuro <gitgitgithub@gmail.com>
2017-08-13 05:17:05 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-
+  #get 'toppages/index'
   #root to: 'tasks#index'
   root to: 'toppages#index'
-  resources :tasks
+  resources :tasks #一時的にON/OFF
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
